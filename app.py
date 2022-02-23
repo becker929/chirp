5af52a9548ac4e470293e8fe559dbb0bd25d484a
Anthony Becker <anthony@visualone.tech>
2022-02-23 11:13:55 -0800

    serve frontend from /chirp route in Flask

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="chirp-frontend/build", static_url_path="/")
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/chirp")
+def chirp():
+    return app.send_static_file("index.html")
