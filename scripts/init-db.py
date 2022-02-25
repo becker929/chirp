29523a3556e60c0056718164ad7bfa45cebc8448
Anthony Becker <anthony@visualone.tech>
2022-02-24 16:28:47 -0800

    update init-db.py to include adding a row

--- scripts/init-db.py
+++ scripts/init-db.py
@@ -1,4 +1,17 @@
+import json
 import sqlite3
+
 con = sqlite3.connect("../server/server/chirp.db")
 cursor = con.cursor()
-cursor.execute("""CREATE TABLE sequences (name varchar(80), data json)""")
+try:
+    cursor.execute("""CREATE TABLE sequences (name varchar(80), data json)""")
+except Exception as e:
+    print(f"exception while creating table:{e}")
+    print("continuing")
+cursor.execute(
+    """
+    INSERT INTO sequences VALUES
+    ('incredible-shelves', :data)
+    """,
+    {"data": json.dumps({"cellsAreActive": [[False] * 12] * 12})},
+)
